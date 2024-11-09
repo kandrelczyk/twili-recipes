c730da18a0f5da40702c3363691608819829f66e
Krzysztof Andrelczyk <cristof@curiana.net>
2024-11-09 23:25:39 +0100

    tested on android

--- src/components/recipes/list.rs
+++ src/components/recipes/list.rs
@@ -111,7 +111,7 @@ pub fn List() -> impl IntoView {
                         </div>
                     }
                 }>
-                    <div class="h-screen w-full overflow-y-scroll">
+                    <div class="h-screen w-full overflow-y-scroll pb-12">
                         <div class="w-full flex flex-row justify-center p-4">
                             <div class="flex flex-row flex-wrap gap-4 pt-4 justify-around">
                                 {move || Suspend::new(async move {
