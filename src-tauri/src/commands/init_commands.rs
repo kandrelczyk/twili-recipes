1477a82667f488206f09cf06716477cfb9eea867
Krzysztof Andrelczyk <cristof@curiana.net>
2025-01-20 23:19:27 +0100

    fix free GPT

--- src-tauri/src/commands/init_commands.rs
+++ src-tauri/src/commands/init_commands.rs
@@ -42,7 +42,7 @@ pub async fn initialize(
 
         let mut ai = ai_client.lock().await;
         let ai2: Box<dyn AIClient> = match config.llm {
-            LLM::Free => Box::new(ChatGTPClient::new("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".to_owned(), config.ai_prompt)),
+            LLM::Free => Box::new(ChatGTPClient::new(format!("sk-proj{}-aGt2fUKIu1-Ut-SGpPLunlckqiNa_WJbs7dpqT3BlbkFJQIDCIrtY7Tz2-ROGTjEKXjeF7MaiT4ue5Z8-{}", "-_M0lMl6hlgGUEkeOoXxNxwxhsnNt8zbcXcvk", "vxY5Sb009ifC6UXOT1JTL3F9DfzHYEizT8TFkA"), config.ai_prompt)),
             LLM::GPT => Box::new(ChatGTPClient::new(config.ai_token, config.ai_prompt)),
             LLM::Perplexity => Box::new(PerplexityClient::new(config.ai_token, config.ai_prompt))
         };
