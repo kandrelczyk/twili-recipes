d6ea8d0f13d3e27ccec85de5b4270b8ee93c8c28
Krzysztof Andrelczyk <cristof@curiana.net>
2025-12-03 08:13:02 +0100

    fix f-droid

--- copy-android.sh
+++ copy-android.sh
@@ -1,2 +1,3 @@
+cp android-files/AndroidManifest.xml src-tauri/gen/android/app/src/main/AndroidManifest.xml
 cp -r android-files/values/ src-tauri/gen/android/app/src/main/res/
 cp -r android-files/mipmap-anydpi-v26/ src-tauri/gen/android/app/src/main/res/
